07bc0627840222a9b103e3ece68293d2380a0e39
jspenliany <penliany00@163.com>
2026-09-13 04:53:13 -0400

    update

--- cnodes/ExtractDetail.py
+++ cnodes/ExtractDetail.py
@@ -1,10 +1,13 @@
 from cstate.PersonState import PersonState
 from langchain_core.messages import HumanMessage
 from cmodels.loadModel import load_local_model
+from logger.log_def import setup_singleton_logger
+log=setup_singleton_logger()
 
 llm = load_local_model()
 
 def extract_memory_node(state: PersonState):
+    log.info("Extracting Memory Node.....begin")
     last_user_msg = state["messages"][-1].content
 
     prompt = f"""Analyze this message: '{last_user_msg}'
@@ -14,4 +17,5 @@ def extract_memory_node(state: PersonState):
 
     response = llm.invoke([HumanMessage(content=prompt)])
     # In a real app, you'd save state['new_facts'] to a Vector DB here
+    log.info(response)
     return {"new_facts": [response.content]}
